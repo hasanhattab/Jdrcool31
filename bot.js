a0b2f6ba394ad89e85cf74219ebd3a417d0cb554
hasanhattab <44847801+hasanhattab@users.noreply.github.com>
2018-11-09 20:14:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "504716682263134218"; // ايدي السررفر
+var channel = "508235582460133376";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('**RedWolf  >  Spaaaaaaaaaaaaaaaam **')
     },305);
 })
 
